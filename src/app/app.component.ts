479178c611f48f569ea589c8741eb74e256eac9a
mkidushim <mkidushim@gmail.com>
2018-03-01 14:45:27 -0800

    joe kidushim

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +6,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  Latitude:number= 34.1477849;
+  Longitude:number= -118.1445155;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBfw0yeuD9I263DpcBHs5H27_C0JYziYeo'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
